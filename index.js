afdf99e6e97e168d7cfef4092e93745ee466d6a5
kdjjps <113160568+kdjjps@users.noreply.github.com>
2024-08-04 11:19:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ dotenv.config({
     path: './.env'
 })
 
+const PORT = process.env.PORT || 8000
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/./index.html')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log('App is Running on port ' + process.env.PORT)
 })
 
